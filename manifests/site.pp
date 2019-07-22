b7f0af39e4c549040f75daf6c2784d673122c41f
Chris Webster <chirs.webster@puppet.com>
2019-07-22 14:25:01 -0700

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -36,7 +36,7 @@ node 'pe-201910-agent.puppetdebug.vlan' {
   file { 'test.txt':
     ensure  => present,
     path    => '/tmp/test.txt',
-    content => $somevar[0],
+    content => $somevar[0][1],
   }
 
   notify { $somevar: }
