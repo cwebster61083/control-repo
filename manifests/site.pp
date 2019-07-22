c6c7ebb4f404f9cd17609087dba80de7c3fe4632
Chris Webster <chirs.webster@puppet.com>
2019-07-22 11:30:18 -0700

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -33,5 +33,10 @@ node default {
 node 'pe-201910-agent.puppetdebug.vlan' {
     $somevar = lookup('profile::default::publicip')
     notify { $somevar: }
+
+    nginx::resource::server { 'kibana.myhost.com':
+      listen_port => 4444,
+      proxy       => 'http://localhost:5555',
+    }
 }
 
