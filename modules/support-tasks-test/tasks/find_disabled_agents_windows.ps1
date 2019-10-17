2f25bcff02daf15ad24600fa5ab72bacb3b33863
Chris Webster <chirs.webster@puppet.com>
2019-10-17 15:11:39 -0700

    asdf

--- modules/support-tasks-test/tasks/find_disabled_agents_windows.ps1
+++
@@ -1,9 +0,0 @@
-$service = Get-WmiObject -Class Win32_Service -Property StartMode -Filter "Name='Puppet'"
-
-if($service.StartMode -eq "Disabled"){
-  echo "Puppet agent is disabled"
-}
-else{
-  echo "Puppet agent is enabled"
-  EXIT 1
-} 
