e1f64954e1ef22321b8a7e10aa905e543804b79f
Chris Webster <chirs.webster@puppet.com>
2019-10-28 08:48:21 -0700

    asdf

---
+++ modules/windows_tasks/manifests/disable_powershell2.pp
@@ -0,0 +1,16 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include windows_tasks::disable_powershell2
+class windows_tasks::disable_powershell2 {
+  dsc {'WindowsOptionalFeature':
+    resource_name => 'windowsfeature',
+    module        => 'PSDesiredStateConfiguration',
+    properties    => {
+      ensure  => 'Disable',
+      name    => 'MicrosoftWindowsPowershellV2',
+  }
+}
+
