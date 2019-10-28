0bba118c5f3a64db7f692b3b79bff54e36ad1751
Chris Webster <chirs.webster@puppet.com>
2019-10-28 16:34:49 -0700

    asdf

--- modules/test/manifests/whoami.pp
+++ modules/test/manifests/whoami.pp
@@ -6,5 +6,5 @@
 #   include test::whoami
 class test::whoami {
   notify { 'I am a test module.': }
-  notify { 'I am here as well do not forget about me!: }
+  notify { 'I am here as well do not forget about me!': }
 }
