c951d76cb713ca3c608fde07af0fa4c87b0ffb30
Chris Webster <chirs.webster@puppet.com>
2019-11-26 15:36:28 -0800

    added windows host

--- manifests/site.pp
+++ manifests/site.pp
@@ -68,7 +68,7 @@ node 'pe-201911-master.puppetdebug.vlan' {
 }
 
 node 'windows.puppetdebug.vlan' {
-    $groups = {'Administrators'}
+    $groups = {'Administrators', 'testing'}
     notify {'I am windows host':}
     create_resources(group, $groups) 
 }
