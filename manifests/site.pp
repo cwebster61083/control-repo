a3c3f8e98d92359bcef072a8912ff120ed0827c0
Chris Webster <chirs.webster@puppet.com>
2019-12-02 16:53:08 -0800

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -103,7 +103,7 @@ node 'elastic.puppetdebug.vlan' {
     config      => {
       #  'xpack.monitoring.collection.enabled' => true,
       'network.host'                 => '0.0.0.0',
-      'http.port'                    => '8200',
+      'http.port'                    => '9200',
       'cluster.initial_master_nodes' => 'elastic.puppetdebug.vlan',
     },
   }
