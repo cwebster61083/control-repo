b216be2aa07a37d31a2457ba5bce43a38b3c8595
Chris Webster <chirs.webster@puppet.com>
2020-01-13 12:18:01 -0800

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -30,12 +30,6 @@ node default {
   #   class { 'my_class': }
 }
 
-node 'agent2.puppetdebug.vlan' {
-  package { 'pe-client-tools':
-    ensure => installed,
-  }
-}
-
 node 'windows2.puppetdebug.vlan' {
   include profile::base
 }
