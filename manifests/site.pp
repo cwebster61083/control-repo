dc8b8d1063969e2ddac02546da2b2c8002afbeea
Chris Webster <chirs.webster@puppet.com>
2020-01-14 15:44:33 -0800

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -30,6 +30,12 @@ node default {
   #   class { 'my_class': }
 }
 
+node 'windowsdc.puppetdebug.vlan' {
+  class {'::puppet_agent':
+    package_version => '6.4.3',
+  }
+}
+
 node 'agent2.puppetdebug.vlan' {
   user { 'testuser':
     ensure     => present,
