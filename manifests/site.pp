e065916ca0377bb40724b92fad0680edf512b504
Chris Webster <chirs.webster@puppet.com>
2020-01-22 09:26:20 -0800

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -110,7 +110,11 @@ node 'replica.puppetdebug.vlan' {
 node 'master.puppetdebug.vlan' {
   notify { 'I am the master': }
 
-  class { 'rampup_profile_puppetmaster':}
+  class { 'rampup_profile_puppetmaster::hiera':
+    owner => 'root',
+    group => 'root',
+  }
+
 
   class { 'java' :
     package => 'java-1.8.0-openjdk-devel',
