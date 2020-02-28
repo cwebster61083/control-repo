dddb5f19503e6315444e76164751aac4c3e59bca
Chris Webster <chirs.webster@puppet.com>
2020-02-28 13:49:05 -0800

    asdf

--- modules/test/manifests/whoami.pp
+++ modules/test/manifests/whoami.pp
@@ -5,7 +5,7 @@
 # @example
 #   include test::whoami
 class test::whoami (
-  String $message = 'Default Message'
+  Sensitive $message = 'Default Message'
 ) {
   notify { $message: }
   notify { 'I am here as well do not forget about me!': }
