395b2fae2b82d44302aa02bfec106838d0d987eb
Chris Webster <chirs.webster@puppet.com>
2020-03-02 10:08:20 -0800

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -166,6 +166,6 @@ node 'master2019.puppetdebug.vlan' {
   exec { 'testexec':
     command => 'echo $(hostname -f) >> /root/hostname',
     path    => ['/usr/bin', '/usr/sbin'],
-    unless  => 'false',
+    unless  => 'true',
   }
 }
