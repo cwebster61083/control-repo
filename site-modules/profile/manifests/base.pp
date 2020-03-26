b443d8ede7d4735b0aab1eca6457fe0ec9cb805a
Chris Webster <chirs.webster@puppet.com>
2020-03-26 13:19:52 -0700

    updated base.pp

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,8 +1,6 @@
 #Defualt profile
 class profile::base {
 
-  #the base profile should include component modules that will be on all nodes
-  include test::whoamia
-  notify {'running from site.pp':}
+  notify {'This is the base profile!':}
 
 }
