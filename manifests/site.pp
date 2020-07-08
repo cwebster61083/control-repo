175b264360fe7086bc39b6b68b518c40e7e885be
Chris Webster <chirs.webster@puppet.com>
2020-07-08 12:48:48 -0700

    adsf

--- manifests/site.pp
+++ manifests/site.pp
@@ -111,6 +111,7 @@ node 'master.puppetdebug.vlan' {
   notify { 'I am the master':
     message => 'This is my MOM!',
   }
+  include puppet_metrics_collector
 
   # #class { 'java' :
   #   package => 'java-1.8.0-openjdk-devel',
