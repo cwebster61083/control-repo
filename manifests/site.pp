0aa8855bac794f8ea2128881dcd9e28755609163
Chris Webster <chirs.webster@puppet.com>
2020-07-27 09:09:38 -0700

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -198,7 +198,7 @@ node 'server2019.webster.prv' {
       'schedule'   => 'daily',
       'start_time' => '23:00'
     }],
-    user      => 'webster\\testuser1',
+    user      => 'webster\\testuser',
     password  => 'Abcd1234',
   }
 }
