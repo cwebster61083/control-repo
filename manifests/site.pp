885a7186beda883eac1637e8c0e91b39c76c3986
Chris Webster <chirs.webster@puppet.com>
2021-07-22 14:26:15 -0700

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -208,6 +208,23 @@ node 'replica.puppetdebug.vlan' {
   # }
 }
 
+node 'replicated.puppetdebug.vlan' {
+  notify { "I am ${fqdn}.": }
+
+  # class { 'java' :
+  #   package => 'java-1.8.0-openjdk-devel',
+  # }
+  # include logstash
+  # file { '/etc/logstash/conf.d/puppetserver-log.conf':
+  #   ensure => file,
+  #   source => 'puppet:///modules/test/puppetserver-log.conf',
+  # }
+  # file { '/etc/logstash/conf.d/console-services-api-access-log.conf':
+  #   ensure => file,
+  #   source => 'puppet:///modules/test/console-services-api-access-log.conf',
+  # }
+}
+
 node 'master.puppetdebug.vlan' {
   notify { 'I am the master':
     message => 'This is my Primary Puppet Server.',
