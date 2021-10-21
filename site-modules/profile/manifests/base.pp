cd0c8b0366ea70658490e61a9b733ee65ff67b0c
Chris Webster <chirs.webster@puppet.com>
2021-10-21 12:46:28 -0700

    asdf

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -3,25 +3,25 @@ class profile::base {
 
   notify {'This is the base profile!':}
 
-  # include puppet_run_scheduler
+  include puppet_run_scheduler
 
-  $runinterval = 30 #minutes
+  # $runinterval = 30 #minutes
 
-  $first_run = fqdn_rand($runinterval)
-  $second_run = $first_run + $runinterval
+  # $first_run = fqdn_rand($runinterval)
+  # $second_run = $first_run + $runinterval
 
-  cron { 'cron.puppet':
-    command => '/opt/puppetlabs/bin/puppet agent -t > /dev/null',
-    user    => 'root',
-    minute  => [ $first_run, $second_run ],
-  }
+  # cron { 'cron.puppet':
+  #   command => '/opt/puppetlabs/bin/puppet agent -t > /dev/null',
+  #   user    => 'root',
+  #   minute  => [ $first_run, $second_run ],
+  # }
 
-  # make sure we haven't started the puppet daemon ever
-  # this may cause the report on an agent that triggers this via a daemonized run to not be submitted
+  # # make sure we haven't started the puppet daemon ever
+  # # this may cause the report on an agent that triggers this via a daemonized run to not be submitted
 
-  service { 'puppet':
-    ensure  => stopped,
-    enable  => false,
-    require => Cron['cron.puppet' ,]
-    }
+  # service { 'puppet':
+  #   ensure  => stopped,
+  #   enable  => false,
+  #   require => Cron['cron.puppet' ,]
+  #   }
 }
