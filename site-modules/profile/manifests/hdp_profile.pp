6397f9774151cc78ec542119db04894a44612b5f
Chris Webster <chirs.webster@puppet.com>
2021-11-12 12:27:42 -0800

    asdf

--- site-modules/profile/manifests/hdp_profile.pp
+++ site-modules/profile/manifests/hdp_profile.pp
@@ -2,7 +2,7 @@ class profile::hdp_profile {
 
   class { 'hdp::app_stack':
           ca_server                    => 'https://clwpe2019-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal:8140',
-          dns_name                     => 'clwpe2019-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal',
+          dns_name                     => 'clw2019-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal',
           image_repository             => 'gcr.io/hdp-gcp-316600',
           minio_image                  => 'gcr.io/hdp-gcp-316600/puppet/minio:RELEASE.2021-07-30T00-02-00Z',
           redis_image                  => 'gcr.io/hdp-gcp-316600/puppet/redis:6.2.4-buster'
