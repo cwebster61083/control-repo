5d9a223eef8dddd7e7c2c11e8c6309fb92dc7d70
Chris Webster <chirs.webster@puppet.com>
2021-11-12 12:30:42 -0800

    sasdf

--- site-modules/profile/manifests/hdp_profile.pp
+++ site-modules/profile/manifests/hdp_profile.pp
@@ -6,7 +6,7 @@ class profile::hdp_profile {
           image_repository             => 'gcr.io/hdp-gcp-316600',
           minio_image                  => 'gcr.io/hdp-gcp-316600/puppet/minio:RELEASE.2021-07-30T00-02-00Z',
           redis_image                  => 'gcr.io/hdp-gcp-316600/puppet/redis:6.2.4-buster'
-          hdp_version                  => 'demo-0.0.2',
+          version                      => 'demo-0.0.2'
           ui_use_tls                   => true,
           ui_cert_files_puppet_managed => false,
       }
