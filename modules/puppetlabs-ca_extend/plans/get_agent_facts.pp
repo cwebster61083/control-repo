dbc629645c04efaf4a72d79c50b6d9050f3655ff
Chris Webster <chirs.webster@puppet.com>
2021-11-18 14:31:03 -0800

    asdf

--- modules/puppetlabs-ca_extend/plans/get_agent_facts.pp
+++
@@ -1,6 +0,0 @@
-# @summary
-#   A plan to work around BOLT-1168 so that one agent failing in apply_prep won't cause the whole plan to fail.
-# @param nodes The targets to run apply_prep on
-plan ca_extend::get_agent_facts(TargetSpec $nodes) {
-  $nodes.apply_prep
-}
