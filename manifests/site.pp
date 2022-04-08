6ad4017ef629a7d2f6f4aed17b960f565de64a25
Chris Webster <chris.webster@puppet.com>
2022-04-08 13:59:19 -0700

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -320,7 +320,7 @@ node 'win-2019-node-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' 
 
 node 'clwpe-lts-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' {
   include puppet_metrics_dashboard::profile::master::install
-  include puppet_metrics_dashboard::profile::master::postgres_access
+  # include puppet_metrics_dashboard::profile::master::postgres_access
   class { 'pe_status_check':
     indicator_exclusions             => ['S0001', 'S0022'],
   }
@@ -328,6 +328,7 @@ node 'clwpe-lts-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' {
 
 node 'clwpe-lts-b5fe62-1.us-west1-b.c.customer-support-scratchpad.internal' {
   include puppet_metrics_dashboard::profile::master::install
+  include puppet_metrics_dashboard::profile::master::postgres_access
 }
 
 node 'clwpe-lts-b5fe62-2.us-west1-a.c.customer-support-scratchpad.internal' {
