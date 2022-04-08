db84f233704bfb10d41eddd90bb7f3149cd7b073
Chris Webster <chris.webster@puppet.com>
2022-04-08 12:37:51 -0700

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -319,7 +319,8 @@ node 'win-2019-node-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' 
 }
 
 node 'clwpe-lts-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' {
+  include puppet_metrics_dashboard::profile::master::install
   class { 'pe_status_check':
     indicator_exclusions             => ['S0001', 'S0022'],
-}
+  }
 }
