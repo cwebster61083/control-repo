813cc2b0c48373351f9bd1e7b6bb145b168c083e
Chris Webster <chris.webster@puppet.com>
2022-04-11 16:43:08 -0700

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -341,4 +341,8 @@ node 'clwpe-lts-b5fe62-5.us-west1-a.c.customer-support-scratchpad.internal' {
 
 node 'clwpedash-lts-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' {
   include puppet_operational_dashboards
+
+  class {'puppet_operational_dashboards::telegraf::agent':
+  collection_interval => '1m',
+}
 }
