8a7c501b8415747b144e06c9998cb91f0a319504
Chris Webster <chris.webster@puppet.com>
2022-04-22 11:20:26 -0700

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -342,3 +342,7 @@ node 'clwpe-lts-b5fe62-5.us-west1-a.c.customer-support-scratchpad.internal' {
 node 'clwpedash-lts-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' {
   include puppet_operational_dashboards
 }
+
+node 'clwelastic-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' {
+  include puppet_logging_dashboard
+}
