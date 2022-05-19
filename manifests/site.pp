688c2791bb1184ebc57a0fdfc230ab3c1ce04bfc
Chris Webster <chris.webster@puppet.com>
2022-05-19 16:16:45 -0700

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -326,7 +326,7 @@ node 'clwpe-lts-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' {
 }
 
 node 'clwpe-lts-b5fe62-1.us-west1-b.c.customer-support-scratchpad.internal' {
-  include puppet_operational_dashboards::profile::postgres_access
+  include puppet_operational_dashboards
 }
 
 node 'clwpe-lts-b5fe62-2.us-west1-a.c.customer-support-scratchpad.internal' {
@@ -337,10 +337,6 @@ node 'clwpe-lts-b5fe62-3.us-west1-c.c.customer-support-scratchpad.internal' {
 
 node 'clwpe-lts-b5fe62-5.us-west1-a.c.customer-support-scratchpad.internal' {
 
-}
-
-node 'clwpe-lts-b5fe62-1.us-west1-b.c.customer-support-scratchpad.internal' {
-  include puppet_operational_dashboards
 }
 
 node 'clwelastic-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' {
