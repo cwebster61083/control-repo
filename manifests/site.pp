9dba05d4c0d628b568f6ee14c4464d0773bd1bde
Chris Webster <chris.webster@puppet.com>
2023-01-17 15:16:41 -0800

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -355,6 +355,7 @@ node 'clw-win2019-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' {
     dsc_name     => 'W3SVC',
     dsc_startuptype => 'Manual',
     dsc_state       => 'Stopped',
+    validation_mode => 'resource',
 } 
 
 }
