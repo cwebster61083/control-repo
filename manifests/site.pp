fa312ee397d5d9a96765c9ef55df189b938cbe52
Chris Webster <chris.webster@puppet.com>
2023-01-17 15:59:55 -0800

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -354,7 +354,7 @@ node 'clw-win2019-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' {
    dsc_service { 'pxp-agent':
     dsc_name            => 'pxp-agent',
     dsc_startuptype     => 'Automatic',
-    dsc_state           => 'Running',
+    dsc_state           => 'Stopped',
     dsc_builtinaccount  => 'LocalSystem',
     # validation_mode     => 'resource',
     
