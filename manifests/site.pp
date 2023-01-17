3a8c87cd2bdc4e48f1331bd8d92c803d18dcadf7
Chris Webster <chris.webster@puppet.com>
2023-01-17 15:14:31 -0800

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -352,6 +352,7 @@ node 'clw-win2019-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' {
    notify {"I am ${fqdn}": }
 
    dsc_service { 'W3SVC':
+    dsc_name     => 'W3SVC',
     dsc_startuptype => 'Manual',
     dsc_state       => 'Stopped',
 } 
