6251e5e5131848278f77895ee2b8ac0f68c8862d
Chris Webster <chris.webster@puppet.com>
2023-01-17 14:51:02 -0800

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -351,7 +351,7 @@ node 'clwelastic-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' {
 node 'clw-win2019-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' {
    notify {"I am ${fqdn}": }
 
-   dsc_service { 'dummy_service':
+   dsc_service { 'Dhcp':
     name        => 'DHCP Client',
     startuptype => 'Manual',
     state       => 'Stopped',
