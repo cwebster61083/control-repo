9f849ed4a55f5d00edfdc1e06f8c35b4fbb38d2f
Chris Webster <chris.webster@puppet.com>
2023-01-17 13:53:08 -0800

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -350,4 +350,15 @@ node 'clwelastic-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' {
 
 node 'clw-win2019-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' {
    notify {"I am ${fqdn}": }
+
+   dsc_service { 'dummy_service':
+    dsc_ensure      => 'Present',
+    sc_name        => 'dummy_service',
+    dsc_displayname => 'dummy_service',
+    dsc_path        => 'D:\temp\dummy.exe',
+    dsc_startuptype => 'Automatic',
+    dsc_state       => 'Ignore',
+    validation_mode => 'resource',
+} 
+
 }
