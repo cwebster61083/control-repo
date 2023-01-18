ee8b78ab95a6b871f9d6103f0121ad4fdeecc4a6
Chris Webster <chris.webster@puppet.com>
2023-01-18 12:47:43 -0800

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -360,11 +360,9 @@ node 'clw-win2019-b5fe62-0.us-west1-c.c.customer-support-scratchpad.internal' {
   #   } 
 
 
-  dsc_service { 'dummy_service':
+  dsc_service { 'pxp-agent':
     dsc_ensure      => 'Present',
-    dsc_name        => 'dummy_service',
-    dsc_displayname => 'dummy_service',
-    dsc_path        => 'C:\temp\dummy.exe',
+    dsc_name        => 'pxp-agent',
     dsc_startuptype => 'Automatic',
     dsc_state       => 'Ignore',
     validation_mode => 'resource',
