4afaf3ac0eecaa4718813d6a71be009bc9516d89
Chris Webster <chris.webster@puppet.com>
2023-10-02 11:45:05 -0700

    asdf

---
+++ modules/test/plans/windows_agent_upgrade.pp
@@ -0,0 +1,20 @@
+plan eh_puppet_agent::windows_agent_upgrade (
+  TargetSpec $targets,
+  String $version,
+  String $source = 'https://artifactory.test.lab:8443/artifactory/chocolatey-installers/windows/puppet7'
+) {
+  $full_source = "${source}/puppet-agent-${version}-x64.msi"
+  $target_objects = get_targets($targets)
+  $facts_result = puppetdb_fact($target_objects)
+  $facts_retrieved_nodes = $facts_result.each |$node, $node_facts| {
+    add_facts(get_target($node), $node_facts)
+  }
+  $result = apply($target_objects, _noop => false) {
+    class { 'puppet_agent':
+      install_options     => ['RESINSTALLMODE="amus"', 'ADDLOCAL=ALL'],
+      package_version     => $version,
+      wait_for_puppet_run => 900000,
+      windows_source      => $source,
+    }
+  }
+}
