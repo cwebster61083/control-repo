51ae855f42e2ac8033e23d29b37f2bf925936a19
Chris Webster <chris.webster@puppet.com>
2023-10-06 11:21:57 -0700

    asdf

--- modules/test/plans/update_puppet2.pp
+++ modules/test/plans/update_puppet2.pp
@@ -6,6 +6,7 @@ plan test::update_puppet2(
   # $primary_facts = run_task('facts', $targets, '_catch_errors' => true).first
 
   run_task ('service', $targets, 'action' => 'status', 'name' => 'Puppet Agent')
+  run_task ('service', $targets, 'action' => 'status', 'name' => 'Puppet PXP Agent')
   # run_command("Get-Service -DisplayName 'Puppet PXP Agent' -ErrorAction SilentlyContinue | Stop-Service", $targets)
 
   # run_task('puppet_agent::install', $targets, 'version' => $version)
