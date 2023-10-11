019480527dabb914b6bc8f78c6b08db17624076b
Chris Webster <chris.webster@puppet.com>
2023-10-11 12:34:10 -0700

    asdf

--- modules/test/plans/update_puppet2.pp
+++ modules/test/plans/update_puppet2.pp
@@ -5,13 +5,13 @@ plan test::update_puppet2(
   # $targets.apply_prep
   # $primary_facts = run_task('facts', $targets, '_catch_errors' => true).first
 
-  run_task ('service', $targets, 'action' => 'stop', 'name' => 'puppet')
-  run_task ('service', $targets, 'action' => 'stop', 'name' => 'pxp-agent')
+  # run_task ('service', $targets, 'action' => 'stop', 'name' => 'puppet')
+  # run_task ('service', $targets, 'action' => 'stop', 'name' => 'pxp-agent')
 
-  run_task('puppet_agent::install', $targets, 'version' => '7.25.0')
+  run_task('puppet_agent::install', $targets, 'version' => '7.25.0', 'stop_service' => 'true')
 
-  run_task ('service', $targets, 'action' => 'start', 'name' => 'puppet')
-  run_task ('service', $targets, 'action' => 'start', 'name' => 'pxp-agent')
+  # run_task ('service', $targets, 'action' => 'start', 'name' => 'puppet')
+  # run_task ('service', $targets, 'action' => 'start', 'name' => 'pxp-agent')
   # run_command("Get-Service -DisplayName 'Puppet PXP Agent' -ErrorAction SilentlyContinue | Stop-Service", $targets)
 
   # run_task('puppet_agent::install', $targets, 'version' => $version)
