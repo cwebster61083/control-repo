e4066fab651709f346870b7032f6e10293e7797e
Chris Webster <chris.webster@puppet.com>
2024-11-21 15:35:48 -0800

    asdf

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -3,6 +3,6 @@ class profile::base (
   $simple = lookup('simple', Hash, 'deep'),
   $complex = lookup('role::name::complex', Hash, 'deep'),
 ) {
-  notify { "${simple}":}
-  notify { "${complex}":}
+  # notify { "${simple}":}
+  # notify { "${complex}":}
 }
